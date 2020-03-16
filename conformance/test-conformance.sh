884433524f2917a4bc6166eb2245c31feb099c9d
Sebastien Leclerc Lavallee <sebastien@studyo.co>
2020-03-15 20:39:25 -0400

    Fix path in test-conformance js

--- conformance/test-conformance.sh
+++ conformance/test-conformance.sh
@@ -11,4 +11,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 $CONF_TEST_PATH --failure_list $DIR/src/jvmMain/failing_tests.txt $DIR/build/install/conformance/bin/conformance
 # TODO: js tests currently fail to run with a `Error: Cannot find module 'kotlinx-serialization-kotlinx-serialization-runtime'` error.
 # Reenable these tests once the error has been fixed.
-#$CONF_TEST_PATH --failure_list $DIR/conformance-js/failing_tests.txt $DIR/conformance-js/run.sh
+# $CONF_TEST_PATH --failure_list $DIR/src/jsMain/failing_tests.txt $DIR/conformance-js/run.sh
