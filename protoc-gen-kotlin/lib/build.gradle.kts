814a092ed0d320a77f39efe50a436e0c94d92784
Sebastien Leclerc Lavallee <sebastien@studyo.co>
2020-04-08 08:20:55 -0400

    Add protoc-gen-kotlin macos implementation and gradle file update

--- protoc-gen-kotlin/lib/build.gradle.kts
+++ protoc-gen-kotlin/lib/build.gradle.kts
@@ -16,34 +16,45 @@ kotlin {
     // For Linux, should be changed to e.g. linuxX64
     // For MacOS, should be changed to e.g. macosX64
     // For Windows, should be changed to e.g. mingwX64
-    // macosX64("macos")
+    macosX64()
 
-    sourceSets["commonMain"].dependencies {
-        implementation(kotlin("stdlib-common"))
-        implementation(project(":runtime"))
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:${Versions.kotlinSerialization}")
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation(project(":runtime"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:${Versions.kotlinSerialization}")
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:${Versions.kotlinSerialization}")
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.12")
+            }
+        }
+
+        val macosX64Main by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:${Versions.kotlinSerialization}")
+            }
+        }
     }
-
-    sourceSets["commonTest"].dependencies {
-        implementation(kotlin("test-common"))
-        implementation(kotlin("test-annotations-common"))
-    }
-
-    sourceSets["jvmMain"].dependencies {
-        implementation(kotlin("stdlib-jdk8"))
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:${Versions.kotlinSerialization}")
-    }
-
-    sourceSets["jvmTest"].dependencies {
-        implementation(kotlin("test"))
-        implementation(kotlin("test-junit"))
-        implementation("junit:junit:4.12")
-    }
-
-//    sourceSets["macosMain"].dependencies {
-//    }
-//    sourceSets["macosTest"].dependencies {
-//    }
 }
 
 tasks {
--- protoc-gen-kotlin/protoc-gen-kotlin-native/src/main/kotlin/pbandk/main.kt
+++
@@ -1,3 +0,0 @@
-fun main(args: Array<String>) {
-    pbandk.gen.main(args)
-}
