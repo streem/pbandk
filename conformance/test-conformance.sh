ebf362b82b74901c69c46f72079925e2fd2ec6d1
Sebastien Leclerc Lavallee <sebastien@studyo.co>
2020-06-10 11:44:59 -0400

    Point Linux conformance test to right executable

--- conformance/test-conformance.sh
+++ conformance/test-conformance.sh
@@ -18,7 +18,7 @@ if [ -z "$1" ] || [ "$1" = "js" ]; then
     $CONF_TEST_PATH --failure_list $DIR/js/failing_tests.txt $DIR/js/run.sh
 fi
 if [ -z "$1" ] || [ "$1" = "linux" ]; then
-    $CONF_TEST_PATH --failure_list $DIR/native/failing_tests.txt $DIR/native/build/bin/macos/debugExecutable/conformance.kexe
+    $CONF_TEST_PATH --failure_list $DIR/native/failing_tests.txt $DIR/native/build/bin/linux/debugExecutable/conformance.kexe
 fi
 if [ "$1" = "macos" ]; then
     $CONF_TEST_PATH --failure_list $DIR/native/failing_tests.txt $DIR/native/build/bin/macos/debugExecutable/conformance.kexe
