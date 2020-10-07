9166415aca05ba0efe7814a34813e22aab8911ee
Gary Peck <gary.peck@streem.pro>
2020-10-07 09:46:46 -0700

    Bump to 0.9.0-SNAPSHOT

--- examples/custom-service-gen/build.gradle.kts
+++ examples/custom-service-gen/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.google.protobuf") version "0.8.12" apply false
 }
 
-val pbandkVersion by extra("0.9.0-rc.2")
+val pbandkVersion by extra("0.9.0-SNAPSHOT")
 
 subprojects {
     repositories {
