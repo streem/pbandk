7d9ad28ae90be7babfc26f0b33df779640198c29
Gary Peck <gary.peck@streem.pro>
2020-10-20 22:21:53 -0700

    Check that expected size of binary message matches the actual size when encoding
    This helps to catch bugs and discrepancies between the `Sizer` code and
    `BinaryMessageEncoder` code.

--- runtime/src/commonMain/kotlin/pbandk/internal/binary/kotlin/KotlinBinaryMessageEncoder.kt
+++ runtime/src/commonMain/kotlin/pbandk/internal/binary/kotlin/KotlinBinaryMessageEncoder.kt
@@ -4,11 +4,14 @@ import pbandk.internal.binary.BinaryMessageEncoder
 import pbandk.internal.binary.ByteArrayMessageEncoder
 
 internal class KotlinBinaryMessageEncoder private constructor(
-    private val writer: ByteArrayWireWriter
+    private val writer: ByteArrayWireWriter,
+    private val expectedSize: Int
 ) : BinaryMessageEncoder(KotlinBinaryWireEncoder(writer)), ByteArrayMessageEncoder {
-    override fun toByteArray() = writer.toByteArray()
+    override fun toByteArray() = writer.toByteArray().also {
+        check(it.size == expectedSize) { "Expected $expectedSize, got ${it.size}" }
+    }
 
     companion object {
-        fun allocate(size: Int) = KotlinBinaryMessageEncoder(ByteArrayWireWriter.allocate(size))
+        fun allocate(size: Int) = KotlinBinaryMessageEncoder(ByteArrayWireWriter.allocate(size), size)
     }
 }
