9312331cda2501c908b6b902ab6cc9b79ed8128f
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2020-11-21 15:22:06 +0100

    include test in build.gradle

--- protoc-gen-kotlin/lib/build.gradle.kts
+++ protoc-gen-kotlin/lib/build.gradle.kts
@@ -54,6 +54,16 @@ tasks {
         kotlinPackage.set("pbandk.gen.pb")
         logLevel.set("debug")
     }
+
+    val generateKotlinTestTypes by registering(KotlinProtocTask::class) {
+        outputDir.set(project.file("src/commonTest/kotlin"))
+        kotlinPackage.set("pbandk.gen.pb")
+        logLevel.set("debug")
+    }
+
+    val generateTestTypes by registering {
+        dependsOn(generateKotlinTestTypes)
+    }
 }
 
 publishing {
