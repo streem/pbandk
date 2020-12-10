7dea33d2d18fcb9d11557305a6a07d668ec96c12
Jeroen Mols <jmols@plaid.com>
2020-12-10 17:07:21 +0100

    Bump Kotlin version to stable 1.4.21

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
     const val binaryCompatibilityValidatorGradlePlugin = "0.2.3"
-    const val kotlin = "1.4.20-M2"
-    const val kotlinCoroutines = "1.3.9"
-    const val kotlinSerialization = "1.0.0"
+    const val kotlin = "1.4.21"
+    const val kotlinCoroutines = "1.4.1"
+    const val kotlinSerialization = "1.0.1"
     const val protobufJava = "3.11.1"
     const val protobufJs = "6.10.1"
     const val springBootGradlePlugin = "2.3.4.RELEASE"
