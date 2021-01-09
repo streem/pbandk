3a42b6884aa7343b8c592d0c659b1e3d9da62bab
Nisheeth Barthwal <nisheeth.barthwal@gmail.com>
2021-01-09 11:27:47 +0100

    use protoc task to generate file descriptors

---
+++ buildSrc/src/main/kotlin/DescriptorProtocTask.kt
@@ -0,0 +1,52 @@
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileCollection
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+import org.gradle.kotlin.dsl.property
+
+@Suppress("UnstableApiUsage")
+open class DescriptorProtocTask : AbstractExecTask<DescriptorProtocTask>(DescriptorProtocTask::class.java) {
+    @InputDirectory
+    val includeDir: DirectoryProperty = project.objects.directoryProperty()
+
+    @OutputDirectory
+    val outputDir: DirectoryProperty = project.objects.directoryProperty()
+
+    @Input
+    val protoc: Property<String> = project.objects.property<String>().apply {
+        convention(
+            project.providers.systemProperty("protoc.path")
+                .map { project.layout.projectDirectory.dir(it).file("bin/protoc") }
+                .map { it.asFile.absolutePath }
+                .orElse("protoc")
+        )
+    }
+
+    @Input
+    val descriptorSetOutput: Property<String> = project.objects.property()
+
+    private val protoFileDir: DirectoryProperty = project.objects.directoryProperty().apply {
+        convention(includeDir)
+    }
+
+    @InputFiles
+    @SkipWhenEmpty
+    val protoFiles: FileCollection = protoFileDir.asFileTree.matching {
+        this.include("*.proto")
+    }
+
+    init {
+        descriptorSetOutput.set("fileDescriptor.protoset")
+    }
+
+    override fun exec() {
+        executable = protoc.get()
+
+        args("--proto_path=${includeDir.get()}")
+        args("--descriptor_set_out=${outputDir.get().file(descriptorSetOutput.get()).asFile.absolutePath}")
+
+        args(protoFiles.map { it.absolutePath })
+
+        super.exec()
+    }
+}
