545cce301f8cd7e9035363b73bc6a4334222b068
Jeroen Mols <jmols@plaid.com>
2021-03-04 15:31:18 +0100

    Rely on Java.util.Base64 by default but fallback to Andorid.util.Base64 when needed

--- runtime/src/androidMain/kotlin/pbandk/internal/Util.kt
+++ runtime/src/androidMain/kotlin/pbandk/internal/Util.kt
@@ -1,8 +1,18 @@
 package pbandk.internal
 
-import android.util.Base64
-
 internal actual object Util : AbstractUtil() {
-  actual override fun base64ToBytes(str: String): ByteArray = Base64.decode(str, Base64.DEFAULT)
-  actual override fun bytesToBase64(bytes: ByteArray): String = Base64.encodeToString(bytes, Base64.DEFAULT)
+    actual override fun base64ToBytes(str: String): ByteArray = try {
+        java.util.Base64.getDecoder().decode(str)
+    } catch (e: Throwable) {
+        // Fallback to Android implementation on platforms below API 26
+        android.util.Base64.decode(str, android.util.Base64.DEFAULT)
+    }
+
+    actual override fun bytesToBase64(bytes: ByteArray): String = try {
+        java.util.Base64.getEncoder().encodeToString(bytes)
+    } catch (e: Throwable) {
+        // Fallback to Android implementation on platforms below API 26
+        android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT)
+    }
 }
+
