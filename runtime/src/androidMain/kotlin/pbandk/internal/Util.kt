a2a5a848b033a233d5c097376d1665dccffbb8f8
Jeroen Mols <jeroen@jeroenmols.com>
2021-03-04 20:39:42 +0100

    Fix failing tests due to Base64 mismatch on Android and Jvm

--- runtime/src/androidMain/kotlin/pbandk/internal/Util.kt
+++ runtime/src/androidMain/kotlin/pbandk/internal/Util.kt
@@ -4,8 +4,8 @@ import android.util.Base64
 
 internal actual object Util : AbstractUtil() {
     actual override fun base64ToBytes(str: String): ByteArray =
-        Base64.decode(str, Base64.NO_WRAP)
+        Base64.decode(str, Base64.DEFAULT)
 
     actual override fun bytesToBase64(bytes: ByteArray): String =
-        Base64.encodeToString(bytes, Base64.NO_WRAP)
+        Base64.encodeToString(bytes, Base64.DEFAULT)
 }
