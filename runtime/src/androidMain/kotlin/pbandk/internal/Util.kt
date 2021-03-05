e6a6c861885e7bb03324fb6434bd97633f986ed8
Jeroen Mols <jeroen@jeroenmols.com>
2021-03-05 08:30:48 +0100

    Revert "Fix failing tests due to Base64 mismatch on Android and Jvm"
    This reverts commit a2a5a848b033a233d5c097376d1665dccffbb8f8.

--- runtime/src/androidMain/kotlin/pbandk/internal/Util.kt
+++ runtime/src/androidMain/kotlin/pbandk/internal/Util.kt
@@ -4,8 +4,8 @@ import android.util.Base64
 
 internal actual object Util : AbstractUtil() {
     actual override fun base64ToBytes(str: String): ByteArray =
-        Base64.decode(str, Base64.DEFAULT)
+        Base64.decode(str, Base64.NO_WRAP)
 
     actual override fun bytesToBase64(bytes: ByteArray): String =
-        Base64.encodeToString(bytes, Base64.DEFAULT)
+        Base64.encodeToString(bytes, Base64.NO_WRAP)
 }
