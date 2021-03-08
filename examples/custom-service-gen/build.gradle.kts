0d6a1f36c2851c7af08d1b729e658fec1147647d
Gary Peck <gary.peck@streem.com>
2021-03-08 08:21:37 -0800

    Bump to 0.10.0-beta.2

--- examples/custom-service-gen/build.gradle.kts
+++ examples/custom-service-gen/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.google.protobuf") version "0.8.12" apply false
 }
 
-val pbandkVersion by extra("0.10.0-SNAPSHOT")
+val pbandkVersion by extra("0.10.0-beta.2")
 
 subprojects {
     repositories {
