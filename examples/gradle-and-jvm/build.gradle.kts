334486cc731df348a8d8a464b56812997da5a754
Gary Peck <gary.peck@streem.com>
2021-03-12 16:58:04 -0800

    Bump to 0.10.0-SNAPSHOT

--- examples/gradle-and-jvm/build.gradle.kts
+++ examples/gradle-and-jvm/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 val protobufVersion by extra("3.11.1")
-val pbandkVersion by extra("0.10.0-beta.3")
+val pbandkVersion by extra("0.10.0-SNAPSHOT")
 
 repositories {
     if (System.getenv("CI") == "true") {
