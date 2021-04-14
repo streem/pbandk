da4a30769ec6a4a6e3216bdea58eb3490a1ed16f
Jeroen Mols <jeroen@jeroenmols.com>
2021-04-14 14:38:59 +0200

    Add required pom.xml attributes

--- buildSrc/src/main/kotlin/configureMavenPom.kt
+++ buildSrc/src/main/kotlin/configureMavenPom.kt
@@ -1,12 +1,19 @@
 import org.gradle.api.publish.maven.MavenPom
 
 fun MavenPom.configureForPbandk() {
+    name.set("Pbandk")
+    description.set("Pbandk is a Kotlin code generator and runtime for Protocol Buffers. It is built to work across multiple Kotlin platforms.")
     url.set("https://github.com/streem/pbandk")
     licenses {
         license {
             name.set("MIT License")
             url.set("https://opensource.org/licenses/MIT")
         }
+    }
+
+    organization {
+        name.set("Streem, Inc.")
+        url.set("https://github.com/streem")
     }
 
     developers {
