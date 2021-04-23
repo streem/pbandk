3380a8bc657c8451e02682fd26db9e90275a5553
Jeroen Mols <jeroen@jeroenmols.com>
2021-04-23 16:12:46 +0200

    Rename variable

--- buildSrc/src/main/kotlin/configureMavenSigning.kt
+++ buildSrc/src/main/kotlin/configureMavenSigning.kt
@@ -2,9 +2,9 @@ import org.gradle.kotlin.dsl.get
 import org.gradle.plugins.signing.SigningExtension
 
 fun SigningExtension.addMavenCentralSigning() {
-    val gpgBase64Key = project.providers.gradleProperty("sonatypePgpKeyBase64").forUseAtConfigurationTime()
+    val pgpBase64Key = project.providers.gradleProperty("sonatypePgpKeyBase64").forUseAtConfigurationTime()
             .orElse(project.providers.environmentVariable("SONATYPE_PGP_KEY_BASE64").forUseAtConfigurationTime())
     val keyPassword = ""
-    useInMemoryPgpKeys(gpgBase64Key.get(), keyPassword)
+    useInMemoryPgpKeys(pgpBase64Key.get(), keyPassword)
     sign(project.tasks["publishToMavenCentralRepository"])
 }
