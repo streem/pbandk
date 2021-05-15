fb8db6a00b1c10843d48391efc476e56baac3265
Daniyar Itegulov <ditegulov@gmail.com>
2021-05-15 13:43:16 +1000

    Make Long-related methods compiler-backend-agnostic

--- runtime/src/jsMain/kotlin/pbandk/protobufjs/Util.kt
+++ runtime/src/jsMain/kotlin/pbandk/protobufjs/Util.kt
@@ -6,8 +6,15 @@ import pbandk.internal.asUint8Array
 internal val Long.protobufjsLong: dynamic
     get() {
         val ret = js("{}")
-        ret.high = this.asDynamic().getHighBits()
-        ret.low = this.asDynamic().getLowBits()
+        // Legacy compiler exposes Long bits via functions, while IR compiler exposes Long bits
+        // via `_high` and `_low` fields
+        if (this.asDynamic().getHighBits !== undefined) {
+            ret.high = this.asDynamic().getHighBits()
+            ret.low = this.asDynamic().getLowBits()
+        } else {
+            ret.high = this.asDynamic()._high
+            ret.low = this.asDynamic()._low
+        }
         return ret
     }
 
@@ -15,7 +22,13 @@ internal fun Long.Companion.fromProtobufjsLong(l: dynamic): Long {
     return if (l.low == null || l.high == null) {
         (l as Int).toLong()
     } else {
-        js("Kotlin").Long.fromBits(l.low, l.high) as Long
+        // Legacy compiler exposes Long-related function in `Kotlin` namespace, while IR compiler
+        // exposes a direct Long constructor
+        if (js("typeof Kotlin") !== "undefined") {
+            js("Kotlin").Long.fromBits(l.low, l.high) as Long
+        } else {
+            js("new Long(l.low, l.high)") as Long
+        }
     }
 }
 
