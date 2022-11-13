c3ca021ed14c33ef8da809e982bc16249729f29a
Gary Peck <gary.peck@streem.com>
2022-11-13 09:07:57 -0800

    Ensure that the output of DescriptorProtocTask is self-contained

--- buildSrc/src/main/kotlin/DescriptorProtocTask.kt
+++ buildSrc/src/main/kotlin/DescriptorProtocTask.kt
@@ -2,6 +2,7 @@
 open class DescriptorProtocTask : ProtocTask() {
     init {
         plugin.set("descriptor_set")
+        protocOptions.add("--include_imports")
         outputFileName.convention("fileDescriptor.protoset")
         protocOptions.set(listOf("--include_imports"))
     }
